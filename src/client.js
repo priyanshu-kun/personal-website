3796f3343b969853d13c59179d73f8768550f166
priyanshu-kun <priyanshushrama709@gmail.com>
2024-05-20 21:46:05 +0530

    deployment

--- src/client.js
+++ src/client.js
@@ -7,6 +7,7 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-02-25', // use current UTC date - see "specifying API version"!
   token: process.env.REACT_APP_SANITY_PROJECT_TOKEN, // or leave blank for unauthenticated usage
+  ignoreBrowserTokenWarning: true,
   useCdn: true, // `false` if you want to ensure fresh data
 })
 
